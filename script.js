2bb351060eb2f8e46db166281aa7560123d18617
rudrasingh36 <144276362+rudrasingh36@users.noreply.github.com>
2023-09-07 01:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky" , this.window.scrollY > 100);
+});
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist1');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navlist.classList.toggle('open')
+};
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navlist.classList.remove('open')
+};
